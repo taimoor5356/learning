19451d8f97d4811f040a2903e84f2d8cdbb3494a
taimoor5356 <mtaimoornaveed@gmail.com>
2024-05-10 01:32:53 +0500

    updated

--- routes/web.php
+++ routes/web.php
@@ -34,10 +34,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::group(['prefix' => '/'], function () {
     Route::get('', [AuthController::class, 'login'])->name('login');
     Route::get('/signup', [AuthController::class, 'signup'])->name('signup');
