725925cc5d9c4b5ef910752d0175b7a92b6cd568
taimoor5356 <mtaimoornaveed@gmail.com>
2024-05-13 04:54:08 +0500

    updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -200,6 +200,34 @@
                             </p>
                         </div>
                     </div>
+                    <div class="social-icons d-flex justify-content-between">
+                        <a href="https://cspsacademy.com">
+                            <i class="text-lg fas fa-globe text-green"></i>
+                        </a>
+                        <a href="https://www.facebook.com/CSPsAcademy">
+                            <i class="text-lg fab fa-facebook text-primary"></i>
+                        </a>
+                        <a href="https://www.instagram.com/cspsacademy/">
+                            <i class="text-lg fab fa-instagram text-danger"></i>
+                        </a>
+                        <a href="https://twitter.com/CSPSAcademy">
+                            <i class="text-lg fab fa-twitter"></i>
+                        </a>
+                        <a href="https://www.youtube.com/c/CivilServicesPreparatorySchoolforCSSPMSAcademy">
+                            <i class="text-lg fab fa-youtube text-red"></i>
+                        </a>
+                        <a href="https://www.tiktok.com/@csps_academy">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="text-dark bi bi-tiktok p-0 m-0" viewBox="0 0 16 16">
+                                <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+                            </svg>
+                        </a>
+                        <a href="csps.css@gmail.com">
+                            <i class="text-lg fab fa-google text-red"></i>
+                        </a>
+                        <a href="https://api.whatsapp.com/send/?phone=923165701593&text&type=phone_number&app_absent=0">
+                            <i class="text-lg fab fa-whatsapp text-success"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-12">
